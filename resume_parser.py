15968b1326915ef6855da20d2e84b7f94f515283
Abishek <abishek20907@gmail.com>
2026-06-20 16:02:05 +0530

    Created AI resume analyzer structure

---
+++ resume_parser.py
@@ -0,0 +1,96 @@
+from PyPDF2 import PdfReader
+
+
+
+def extract_text(file):
+
+    reader = PdfReader(file)
+
+    text = ""
+
+
+    for page in reader.pages:
+
+        text += page.extract_text()
+
+
+    return text
+
+
+
+
+
+def analyze_resume(text):
+
+
+    skills = [
+
+        "python",
+        "machine learning",
+        "data science",
+        "html",
+        "css",
+        "javascript",
+        "sql",
+        "flask"
+
+    ]
+
+
+    found = []
+
+
+    missing = []
+
+
+
+    text = text.lower()
+
+
+
+    for skill in skills:
+
+
+        if skill in text:
+
+            found.append(skill)
+
+        else:
+
+            missing.append(skill)
+
+
+
+    score = int(
+        (len(found) / len(skills)) * 100
+    )
+
+
+
+    recommendations = []
+
+
+    for skill in missing:
+
+        recommendations.append(
+            "Learn " + skill
+        )
+
+
+
+    return {
+
+
+        "score": score,
+
+
+        "found": found,
+
+
+        "missing": missing,
+
+
+        "recommendations": recommendations
+
+
+    }
